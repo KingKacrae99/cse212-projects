831a3e972b38b144612ab07b1e21e22a69504672
kingkacrae99 <kacrae9@gmail.com>
2026-01-17 04:04:07 +0100

    week01 Completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,14 +6,30 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    /// 
+    /// <MultiplesOf function Plan>
+    /// create an array with a size equal to the number of multiples
+    /// Loop througj the array 
+    /// For each index, calculate the multiple
+    /// Store the calculated value in the array 
+    /// Return the array 
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
+        //create an array with a size equal to the number of multiples
+        double[] numbers = new double[length];
+
+        // Loop through the array 
+        for (int i = 0; i < length; i++)
+        {
+            // Store the calculated value 
+            numbers[i] = number * (i + 1);
+        }
+
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        return numbers; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +45,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Find where the last 'amount' elements begin
+        int startIndex = data.Count - amount;
+
+        // Step 2: Copy the last 'amount' elements
+        List<int> temp = data.GetRange(startIndex, amount);
+
+        // Remove the last 'amount' elements from the original list
+        data.RemoveRange(startIndex, amount);
+
+        //Insert those elements at the front of the list
+        data.InsertRange(0, temp);
+
+        
     }
 }
